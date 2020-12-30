dc485bc370e2cc64ddf3f06be70006730f012b35
junseok <wnstjr6467@gmail.com>
2020-12-30 21:14:07 +0900

    랜딩페이지 메인이미지 출력

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
+export const API_KEY = '9303cdea3d83d652a4ac005be45ca2a7';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage'
 
 function LandingPage() {
+
+    const [Moives, setMoives] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+
+
+    useEffect(() => {
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en_us&page=1`;
+        fetch(endPoint)
+            .then(res => res.json())
+            .then(res => {
+                setMoives([res.results])
+                setMainMovieImage(res.results[0])
+            })
+    })
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+            {MainMovieImage &&
+                <MainImage image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.title}
+                    text={MainMovieImage.overview}
+                />
+            }
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movie by latest</h2>
+                <hr />
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button>Load More</button>
+            </div>
+        </div>
     )
 }
 
