2b6c7224502c0d9cf08c7b977fcbdb55acb41771
junseok <wnstjr6467@gmail.com>
2020-12-31 13:08:32 +0900

    Load more Button

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,22 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage'
-
+import GridCards from '../commons/GridCards'
+import { Row } from 'antd'
 function LandingPage() {
 
-    const [Moives, setMoives] = useState([])
+    const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
-
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en_us&page=1`;
+        fetchMovies(endPoint)
+    }, [])
+
+    const fetchMovies = (endPoint) => {
         fetch(endPoint)
             .then(res => res.json())
             .then(res => {
-                setMoives([res.results])
+                console.log(res.results)
+                setMovies([...Movies, ...res.results])
                 setMainMovieImage(res.results[0])
+                setCurrentPage(res.page)
             })
-    })
+    }
+
+    const loadMoreItems = () => {
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en_us&page=${CurrentPage + 1}`;
+        fetchMovies(endPoint)
+    }
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
@@ -30,9 +42,24 @@ function LandingPage() {
             <div style={{ width: '85%', margin: '1rem auto' }}>
                 <h2>Movie by latest</h2>
                 <hr />
+                <Row gutter={[16, 16]}>
+
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieID={movie.id}
+                                movieName={movie.title}
+                            />
+                        </React.Fragment>
+                    ))}
+
+                </Row>
+
             </div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button>Load More</button>
+                <button onClick={loadMoreItems}>Load More</button>
             </div>
         </div>
     )
