1f0999b736a17de9308c991a2ae878bd978c554d
junseok <wnstjr6467@gmail.com>
2021-01-01 16:23:13 +0900

    favorite Page

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { Favorite } = require('../models/Favorite')
 
+
 router.post('/favoriteNumber', (req, res) => {
 
     // mongoDB에서 favorite 숫자를 가져오기
     Favorite.find({ "movieId": req.body.movieId })
         .exec((err, info) => {
             if (err) return res.status(400).send(err)
-
             res.status(200).json({ success: true, favoriteNumber: info.length })
         })
     // 그다음에 프론트에 다시 숫자 정보를 보내주기
@@ -30,5 +30,39 @@ router.post('/favorited', (req, res) => {
         })
     // 그다음에 프론트에 다시 숫자 정보를 보내주기
 })
+
+
+router.post('/addFavorite', (req, res) => {
+    const favorite = new Favorite(req.body);
+    favorite.save((err, doc) => {
+        if (err) return res.state(400).send(err)
+        return res.status(200).json({ success: true })
+    })
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+    Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+        .exec((err, doc) => {
+            if (err) return res.status(400).send(err)
+            res.status(200).json({ success: true, doc })
+        })
+})
+
+router.post('/getFavorite', (req, res) => {
+    Favorite.find({ 'userFrom': req.body.userFrom })
+        .exec((err, favorites) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, favorites })
+        })
+})
+
+router.post('/deleteFromFavorite', (req, res) => {
+    Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+        .exec((err, result) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({ success: true })
+        })
+})
+
 
 module.exports = router;
